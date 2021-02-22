693591c9d2fd3916f0e6db63bf149ed6ee6a1871
Ashish Nimrot <ashishnimrot@gmail.com>
2021-02-22 11:55:55 +0530

    Add default user for login

--- server/database/seeders/DatabaseSeeder.php
+++ server/database/seeders/DatabaseSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,6 +16,15 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-         User::factory(10)->create();
+        User::factory(10)->create();
+        User::firstOrCreate(
+            ['email' => 'ashish.nimrot@world.com'],
+            [
+                'name' => 'Ashish Nimrot',
+                'password' => Hash::make('password'),
+                'email_verified_at' => now(),
+                'remember_token' => Str::random(10),
+            ]
+        );
     }
 }
